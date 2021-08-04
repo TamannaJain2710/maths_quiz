b80cd098964a8853ef0b8d94e80a0a9059d2d411
TamannaJain2710 <thecreativefeather725@gmail.com>
2021-08-04 21:03:06 +0530

    w

--- button.js
+++ button.js
@@ -1,17 +1,17 @@
 class Button{
     constructor(){
-        this.next = createButton('next');
+       /* this.next = createButton('next');
         this.next.position(displayWidth/2+50,displayHeight/2+130);
         this.next.style('background', 'lightgrey');
         this.next.style('font-size', '15px');
-        this.next.hide()
+        this.next.hide()*/
         this.check = createButton('Check');
         this.check.position(displayWidth/2+50,displayHeight/2+80);
         this.check.style('background', 'lightgrey');
         this.check.style('font-size', '20px'); 
-        this.input = createInput(' ')
+        this.input = createInput('answer')
         this.input.position(displayWidth/2+50,displayHeight/2+40);
-        this.input.style('background', 'white'); 
+        this.input.style('background', 'white');
     }
     solving(){
         this.check.mousePressed(() => { 
@@ -20,7 +20,7 @@ class Button{
                 text("correct",displayWidth/2+50,displayHeight/2+120)
                 this.check.hide();
                 this.input.hide();
-                this.next.show();
+                //this.next.show();
             }else if(this.input.value() != q){
                 background("red");
                 text("Wrong",displayWidth/2+50,displayHeight/2+120)
